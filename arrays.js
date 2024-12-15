e87e2911f2d0b9447bbc16a114341aaa08238af4
olawaleQA <olawaleolowo19@gmail.com>
2024-12-15 18:33:25 +0100

    first commit

---
+++ arrays.js
@@ -0,0 +1,12 @@
+const friends = ["damilola", "olaleye","maxwell", "isaac",];
+
+// friends[0] = "moses"
+
+// console.log(friends);
+// console.log(friends[0]);
+// console.log(friends[1]);
+// console.log(friends[2]);
+// console.log(friends[3]);
+
+let numOfFriends = friends.length
+console.log(numOfFriends);
